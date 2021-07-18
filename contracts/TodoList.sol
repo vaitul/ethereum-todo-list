2fbb5fa4cbefed87e9189b80123df1c93303a3e2
Vaitul Bhayani <39405950+vaitul@users.noreply.github.com>
2021-07-18 21:05:31 +0530

    added html code

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,5 +1,32 @@
 pragma solidity ^0.5.0;
 
 contract TodoList {
+    
+    struct Task{
+        uint id;
+        string content;
+        bool completed;
+    }
+
     uint public taskCount = 0;
+    mapping(uint => Task) public tasks;
+
+    constructor() public {
+        newTask("Do workout");
+        newTask("Read book");
+    }
+
+    function newTask(string memory ct) public{
+        Task memory newT = Task(++taskCount,ct,false);
+        tasks[taskCount] = newT;
+    } 
+
+    function completeTask(uint _id) public {
+        for (uint256 index = 0; index < taskCount; index++) {
+            if(tasks[index].id == _id){
+                tasks[index].completed = true;
+                break;
+            }
+        }
+    }
 }
